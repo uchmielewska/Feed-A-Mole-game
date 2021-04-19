d40c5a781334f998405165f0c7c954ba09e43a53
Urszula Chmielewska <62251549+uchmielewska@users.noreply.github.com>
2021-04-19 22:21:11 +0200

    Update moles.js

--- moles.js
+++ moles.js
@@ -1,6 +1,8 @@
+let score = 0;
+
 //status lasts for 1s
-function getSadInterval() {
-    return Date.now() + 1000
+function getSadHappyInterval() {
+    return Date.now() + 1000;
 }
 //status lasts for numebr from interval (2s to 20s)
 function getGoneInterval() {
@@ -10,65 +12,68 @@ function getGoneInterval() {
 function getHungryInterval() {
     return Date.now() + Math.floor(Math.random() * 3000) + 2000;
 }
+function getKingStatus() {
+    return Math.random() < 0.1;
+}
 
-const mole = [
+const moles = [
     {
         status: "sad",
-        next: getSadInterval(),
+        next: getSadHappyInterval(),
         king: false,
         node: document.querySelector('#hole-0')
     },
     {
         status: "sad",
-        next: getSadInterval(),
+        next: getSadHappyInterval(),
         king: false,
         node: document.querySelector('#hole-1')
     },
     {
         status: "sad",
-        next: getSadInterval(),
+        next: getSadHappyInterval(),
         king: false,
         node: document.querySelector('#hole-2')
     },
     {
         status: "sad",
-        next: getSadInterval(),
+        next: getSadHappyInterval(),
         king: false,
         node: document.querySelector('#hole-3')
     },
     {
         status: "sad",
-        next: getSadInterval(),
+        next: getSadHappyInterval(),
         king: false,
         node: document.querySelector('#hole-4')
     },
     {
         status: "sad",
-        next: getSadInterval(),
+        next: getSadHappyInterval(),
         king: false,
         node: document.querySelector('#hole-5')
     },
     {
         status: "sad",
-        next: getSadInterval(),
+        next: getSadHappyInterval(),
         king: false,
         node: document.querySelector('#hole-6')
     },
     {
         status: "sad",
-        next: getSadInterval(),
+        next: getSadHappyInterval(),
         king: false,
         node: document.querySelector('#hole-7')
     },
     {
         status: "sad",
-        next: getSadInterval(),
+        next: getSadHappyInterval(),
         king: false,
         node: document.querySelector('#hole-8')
     },
     {
         status: "sad",
-        next: getSadInterval(),
+        next: getSadHappyInterval(),
         king: false,
         node: document.querySelector('#hole-9')
     }
@@ -77,9 +82,14 @@ const mole = [
 function getNextStatus(mole) {
     switch (mole.status) {
         case "sad":
-            mole.next = getSadInterval();
+        case "fed":
+            mole.next = getSadHappyInterval();
             mole.status = "leaving";
-            mole.node.children[0].src = './mole-game/mole-leaving.png';
+            if (mole.king) {
+                mole.node.children[0].src = './mole-game/king-mole-leaving.png';
+            } else {
+                mole.node.children[0].src = './mole-game/mole-leaving.png';
+            }
             break;
         case "leaving":
             mole.next = getGoneInterval();
@@ -88,16 +98,25 @@ function getNextStatus(mole) {
             break;
         case "gone":
             mole.status = "hungry";
+            mole.king = getKingStatus();
             mole.next = getHungryInterval();
             mole.node.children[0].classList.add("hungry");
             mole.node.children[0].classList.remove("gone");
-            mole.node.children[0].src = './mole-game/mole-hungry.png';
+            if (mole.king) {
+                mole.node.children[0].src = './mole-game/king-mole-hungry.png';
+            } else {
+                mole.node.children[0].src = './mole-game/mole-hungry.png';
+            }
             break;
         case "hungry":
             mole.status = "sad";
-            mole.next = getSadInterval();
+            mole.next = getSadHappyInterval();
             mole.node.children[0].classList.remove("hungry");
-            mole.node.children[0].src = './mole-game/mole-sad.png';
+            if (mole.king) {
+                mole.node.children[0].src = './mole-game/king-mole-sad.png';
+            } else {
+                mole.node.children[0].src = './mole-game/mole-sad.png';
+            }
             break;
     }
 }
@@ -106,21 +125,35 @@ function feed(event) {
     if (event.target.tagName !== 'IMG' || !event.target.classList.contains("hungry")) {
         return;
     }
-    const mole = mole[parseInt(event.target.dataset.index)]
+    const mole = moles[parseInt(event.target.dataset.index)]
 
     mole.status = "fed";
-    mole.next = getSadInerval();
-    mole.node.children[0].src = './mole-game/mole-fed.png';
+    mole.next = getSadHappyInterval();
+    if (mole.king) {
+        mole.node.children[0].src = './mole-game/king-mole-fed.png';
+        score += 2;
+    } else {
+        mole.node.children[0].src = './mole-game/mole-fed.png';
+        score++;
+    }
     mole.node.children[0].classList.remove("hungry");
+
+    if (score >= 10)
+        win();
+}
+
+function win() {
+    document.querySelector('.bg').classList.add("hide");
+    document.querySelector('.win').classList.remove("hide");
 }
 
 let runAgainAt = Date.now() + 100;
 function nextFrame() {
     const now = Date.now();
     if (runAgainAt <= now) {
-        for (let i = 0; i < mole.length; i++) {
-            if (mole[i].next <= now) {
-                getNextStatus(mole[i]);
+        for (let i = 0; i < moles.length; i++) {
+            if (moles[i].next <= now) {
+                getNextStatus(moles[i]);
             }
         }
         runAgainAt = now + 100;
